740c2b5a76cb6c2ea0f162e35a4db9167330cbed
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2019-11-14 19:17:00 +0100

    problemas con el array

--- inventario.php
+++ inventario.php
@@ -2,7 +2,7 @@
 session_id('inventario');
 session_start(); 
 echo session_name().'<br>'. session_id();
-$_SESSION=array();
+//$_SESSION=array();
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,65 +25,92 @@ $_SESSION=array();
 			<label>Descripción: </label>
 			<textarea id="desc" name="descripcion" type="text" placeholder="Introduce una descripción" required></textarea>
 			<span><?php 
-					if (!preg_match("/^[a-zA-Z0-9]*$/",$_GET['descripcion'])){
-						echo 'Caracteres no permitidos';
-				}
+			if (!preg_match("/^[a-zA-Z0-9]*$/",$_GET['descripcion'])){
+				echo 'Caracteres no permitidos';
+			}
 			?>
 
-			</span>
-			<br>
-			<br>
-			<label>Precio</label>
-			<input id="precio" name="precio" type="number" placeholder="Introduce el precio" required>
-			<span><?php 
-					if(empty($_GET['precio'])){
-						echo 'Precio requerido';
-					}?>
+		</span>
+		<br>
+		<br>
+		<label>Precio</label>
+		<input id="precio" name="precio" type="number" placeholder="Introduce el precio" required>
+		<span><?php 
+		if(empty($_GET['precio'])){
+			echo 'Precio requerido';
+		}?>
 
-			</span>
-			<br>
-			<br>
-			<input type="submit" name="submit" value="Guardar">
-		</fieldset>
-	</form>
+	</span>
+	<br>
+	<br>
+	<input type="submit" name="submit" value="Guardar">
+</fieldset>
+</form>
 
 <div>
 	<?php
 
 	/**Comprueba si se ha pulsado el boton submit*/
 	if (!empty($_GET["submit"])){
-		/**$codigo=strtoupper($_GET['codigo']);
+		$codigo=strtoupper($_GET['codigo']);
 		$descripcion=strtolower($_GET['descripcion']);
 		$precio=$_GET['precio'];
 		/**Funcion strtoupper para convertir la cadena "código" a mayusculas */
 
 		if (!empty($_GET['codigo']) && !empty($_GET['descripcion']) && !empty($_GET['precio'])) {
 			
-			$_SESSION['inventario'][$_GET['codigo']]=array(
+			$_SESSION['inventario'][$_GET['codigo']]=[
 				'descripcion'=>$_GET['descripcion'],
 				'precio'=>$_GET['precio']
-			);
+			];
 
+			foreach ($_SESSION['inventario'] as $codigo) {
+				sort($codigo);
+				$clong=count($codigo);
 
+				for($x = 0; $x < $clong; $x++) {
+					echo $codigo[$x];
+					echo "<br>";
+				}
+				
+			}
+			
+
+			
 			var_dump($_SESSION['inventario']);
-			var_dump($_GET['codigo']);
-			echo $_GET['codigo'];
-			echo '<br>';
-			echo $_SESSION['inventario'][$_GET['codigo']]['descripcion'];
-			echo '<br>';
-			echo $_SESSION['inventario'][$_GET['codigo']]['precio'];
+			//var_dump($_GET['codigo']);
+			//var_dump(each($_GET['codigo']));
+			//echo $_GET['codigo'];
+			//echo '<br>';
+			//echo $_SESSION['inventario'][$_GET['codigo']]['descripcion'];
+			//echo '<br>';
+			//echo $_SESSION['inventario'][$_GET['codigo']]['precio'];
 
 		
+			/*foreach ($_SESSION['invetario'] as $codigo => $datos){
+				echo $codigo;
+				foreach ($datos as $key=>$valor) {
+					echo $valor;
+				}
 
+			} 
+			function mostrarDatos($valor, $key){
+				echo $key . " -> $valor" . "<br>";
+			}
 
-				foreach ($_SESSION['invetario'] as $codigo => $datos){
-					foreach ($datos as $value) {
-						
-					}
-
-				} 
 			
+			/*foreach ($_SESSION['invetario'] as $codigo =>$detalles) {
+				echo '<h1> $codigo </h1>';
+				foreach ($detalles as $clave=>$valor) {
+					echo '<p> $clave: $valor </p>';
+					
+				}
 				
+			}*/
+
+			//array_walk($_SESSION['inventario'][$_GET['codigo']], mostrarDatos);
+			
+
 		}
 
 		
