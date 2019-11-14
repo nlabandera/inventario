d357037e699dec63c7dcc0a23a0072bf5bf40786
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2019-11-14 20:26:44 +0100

    no funciona borrado

--- inventario.php
+++ inventario.php
@@ -11,55 +11,68 @@ echo session_name().'<br>'. session_id();
 	<meta charset="utf-8">
 </head>
 <body>
+	<div>
+		<form action="" action="GET">
+			<fieldset>
+				<legend>Añade producto</legend>
+				<label>Código </label>
+				<input id="cod" name="codigo" type="text" placeholder="Introduce el código" required>
+				<span><?php 
+							if (!preg_match("/^[a-zA-Z0-9]*$/", $_GET['codigo'])){
+								echo 'Código incorrecto';
+							}
+						?>
+						
+				</span> 
+				<br>
+				<label>Descripción: </label>
+				<textarea id="desc" name="descripcion" type="text" placeholder="Introduce una descripción" required></textarea>
+				<span><?php 
+							if (!preg_match("/^[a-zA-Z0-9]*$/",$_GET['descripcion'])){
+								echo 'Caracteres no permitidos';
+							}
+						?>
 
-	<form action="" action="GET">
-		<fieldset>
-			<legend>Añade producto</legend>
-			<label>Código </label>
-			<input id="cod" name="codigo" type="text" placeholder="Introduce el código" required>
-			<span><?php if (!preg_match("/^[a-zA-Z0-9]*$/", $_GET['codigo'])){
-				echo 'Código incorrecto';
-			}
-			?></span> 
-			<br>
-			<label>Descripción: </label>
-			<textarea id="desc" name="descripcion" type="text" placeholder="Introduce una descripción" required></textarea>
-			<span><?php 
-			if (!preg_match("/^[a-zA-Z0-9]*$/",$_GET['descripcion'])){
-				echo 'Caracteres no permitidos';
-			}
-			?>
-
-		</span>
-		<br>
-		<br>
-		<label>Precio</label>
-		<input id="precio" name="precio" type="number" placeholder="Introduce el precio" required>
-		<span><?php 
-		if(empty($_GET['precio'])){
-			echo 'Precio requerido';
-		}?>
-
-	</span>
-	<br>
-	<br>
-	<input type="submit" name="submit" value="Guardar">
-</fieldset>
-</form>
+				</span>
+				<br>
+				<br>
+				<label>Precio</label>
+				<input id="precio" name="precio" type="number" placeholder="Introduce el precio" >
+				<br>
+				<br>
+				<input type="submit" name="submit" value="Guardar">
+			</fieldset>
+		</form>
+	</div>
 
 <div>
 	<?php
 	/**Comprueba si se ha pulsado el boton submit*/
 	if (!empty($_GET["submit"])){
+		/**Funcion strtoupper para convertir la cadena "código" a mayusculas */
 		$codigo=strtoupper($_GET['codigo']);
+		/**Funcion strtolower para convertir la cadena "descripcion" a minusculas */
 		$descripcion=strtolower($_GET['descripcion']);
 		$precio=$_GET['precio'];
-		/**Funcion strtoupper para convertir la cadena "código" a mayusculas */
+		
 
-		if (!empty($codigo) && !empty($descripcion) && !empty($precio)) {
+		if (!empty($codigo) and preg_match("/^[a-zA-Z0-9]*$/", $codigo) and !empty($descripcion) and preg_match("/^[a-zA-Z0-9]*$/", $descripcion)){
+
+			if (array_key_exists($codigo, $_SESSION['inventario']) and $precio=='-1') {
+					unset($_SESSION['inventario'][$codigo]);
+					
+					foreach ($_SESSION['inventario'] as $codigo=>$detalles) {
+						echo $codigo.'<br>';
+				
+						foreach ($detalles as $clave=>$info) {
+						echo $clave.':'. $info .'<br>';
+					
+						}
+				
+					}
+			}
 			
-			
-			
+
 			$_SESSION['inventario'][$codigo]=[
 				'descripcion'=>$descripcion,
 				'precio'=>$precio
@@ -75,9 +88,11 @@ echo session_name().'<br>'. session_id();
 				
 			}
 			
+			
 			//var_dump($_SESSION['inventario']);
 			
 		}
+		
 
 	}
 	//session_destroy();
